c4961630db990a2876c2c336e0c80fc1368f4176
Victor Henrique Silva <henrictor@hotmail.com>
2021-09-14 20:23:43 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CSharp_Study
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int j = 0;
+            string[] list = {"Rick", "Joao", "Pedro"};
+            // Search for Joao inside the list
+            foreach (string i in list) {
+                
+                if (list[j] == "Joao") {
+                    Console.WriteLine(list[j]);
+                    break;
+                }
+               j++; 
+            }
+        }
+    }
+}
